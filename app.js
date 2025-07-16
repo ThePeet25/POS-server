a2f50be2e8f5f362fd9a17091e18676ca4248a0f
ThePeet25 <pbuythiy25@gmail.com>
2025-07-16 19:31:05 +0700

    add cors for test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser')
+const cors = require("cors");
 
 const users = require('./src/api/routes/users.route')
 
@@ -13,7 +14,15 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONT_END]
+}));
+
 app.use('/user', users);
+app.get('/', (req, res) => {
+    res.json({ messsage: 'fuck you'});
+})
 
 app.listen(PORT, () => {
     console.log("App running on",PORT);
