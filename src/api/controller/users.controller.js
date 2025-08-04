e58295ef77854c463a25bc41d2a780369240923e
thePeet25 <pbuythiy25@gmail.com>
2025-08-04 23:34:38 +0700

    fix cookie can't get in frontend2

--- src/api/controller/users.controller.js
+++ src/api/controller/users.controller.js
@@ -74,7 +74,6 @@ exports.loginUser = async (req, res) => {
             secure: process.env.NODE_ENV === 'production', // ใช้ secure: true เฉพาะใน Production (เมื่อใช้ HTTPS)
             maxAge: 1000*60*15, // 1 ชั่วโมง (ในหน่วยมิลลิวินาที)
             sameSite: 'None', // แนะนำ: ป้องกัน CSRF (Cross-Site Request Forgery)
-            domain: process.env.FRONT_END,
         });
 
         res.status(result.status).json({ message: 'Login successful!!' });
